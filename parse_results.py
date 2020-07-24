64b0fbfb5814ea177b249e27053f30ba22bc3f22
jules <jules.vautier@sewan.fr>
2020-07-24 14:55:35 +0200

    Add parse results script

---
+++ parse_results.py
@@ -0,0 +1,20 @@
+import json
+import re
+from pprint import pprint
+
+regex = "http.*?\.(?:png|jpg|jpeg|gif))"
+op_regex = "http:\/\/image.noelshack.com\/fichiers\/(.*?\.(?:png|jpg|jpeg|gif))"
+
+
+def main():
+    with open('results.json', 'r') as fp:
+        results = fp.read()
+        stickers = re.findall(op_regex, results)
+        print(len(stickers))
+        with open('parsed_results.json', 'w+') as parsed_fp:
+            parsed_fp.write(json.dumps(stickers))
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
